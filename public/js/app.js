a04d4b4abc6c5aab1b9a299eefa0939eba0d619b
John Gorman <johngorm@usc.edu>
2017-06-15 21:52:58 -0500

    Add comment section and full form to modal

--- public/js/app.js
+++ public/js/app.js
@@ -11,10 +11,11 @@ var span = $(".close")[0];
 
 //Function to create submission for artical modal
 function createForm(articleId){
-    let $form = $(`<form action="/article/${articleId}" method="POST">`);
-    let $comment = $('<input type="text"  id="comment-text" name="article-comment"> <br> ');
+    let $form = $(`<form action="/articles/${articleId}" method="POST">`);
+    let $commentTitle = $('<input type="text" name="title" id="comment-text" name="article-comment" required> <br> ');
+     let $commentMessage = $('<input type="text" name="message" id="comment-text" name="article-comment" required> <br> ');
     let $commentBtn = $('<button class="btn btn-md btn-info" type="submit">Comment</button>');
-    $form.append($comment);
+    $form.append($commentTitle).append($commentMessage).append($commentBtn);
     return $form;
 }
 
@@ -33,16 +34,37 @@ $('div.container').on("click", '.btn-comment', function(){
     	method: 'GET'
     }).done(function(article, status){
         let title = $("<h2>").html(article.title);
-        let closeBtn =" <span class='close'>&times;</span>"
+        let closeBtn =" <span class='close'>&times;</span>";
         let id_num = $("<span>").html("ID: " + article._id);
-        header.append(title).prepend(closeBtn);
-        body.append(id_num);
+        let $commentSection = $('<div>').attr('id', 'commentSection').addClass('well').text('Comments');
+        
        
-        body.append(createForm(article._id));
+
+        if(article.comment.length){
+            console.log(article.comment);
+            for(let doc in article.comment){
+                let currentCom = article.comment[doc];
+                let $comm = $('<div>').addClass('comment');
+                //Get a title from the database or create one for the client that
+                let $commTitle = $('<h3>').text(doc.title);
+                let $commBody = $('<p>').text(doc.message);
+                let $deleteCommBtn = $(`<button class="btn btn-lg btn-danger btn-delete-comm" data-commentID=${doc._id}>&times;</button>`);
+                $comm.append($commTitle).append($commBody);
+                $commentSection.append($comm);
+            }
+        }
+
+        header.append(title).prepend(closeBtn).append(id_num);
+        body.append($commentSection).append(createForm(article._id));
         modal.css('display', 'block');
     	
     })
-})
+});
+
+
+$('div#commentSection').on('click', '.btn-delete-comm', function(){
+
+});
 
 
 // When the user clicks on <span> (x), close the modal
